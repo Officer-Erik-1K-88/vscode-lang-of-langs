e1863a44147fb8f7e9f7f96b3f01d03947b33b47
Officer-Erik-1K-88 <oficer.erik.k@gmail.com>
2025-09-12 12:04:11 -0500

    added comments

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,10 @@ const webpack = require('webpack');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-    target: 'webworker',
-    entry: './src/extension.ts',
+    target: 'webworker', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
         filename: 'extension.js',
         libraryTarget: 'commonjs2',
@@ -20,9 +21,11 @@ const config = {
     },
     devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
     externals: {
-        vscode: 'commonjs vscode'
+        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
     },
     resolve: {
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
         mainFields: ['main'],
         extensions: ['.ts', '.js']
     },
@@ -38,6 +41,9 @@ const config = {
                 ]
             }
         ]
-    }
+    },
+    infrastructureLogging: {
+        level: "log", // enables logging required for problem matchers
+    },
 };
 module.exports = config;
